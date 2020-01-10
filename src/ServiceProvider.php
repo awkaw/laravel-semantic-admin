1da900b98d031d9842356e170f10e401bcd14c02
Alexander Krupnov <dev@uniqway.ru>
2020-01-10 10:20:25 +0300

    Create ServiceProvider.php

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace LaravelSemanticAdmin;
+
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider{
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+
+	}
+}
